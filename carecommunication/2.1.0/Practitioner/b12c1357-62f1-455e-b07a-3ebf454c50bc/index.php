2b1447d5deaf3c73e92c17a0ba8303b9178c1cfc
tmsMedcom <88831880+tmsMedcom@users.noreply.github.com>
2023-09-15 12:52:15 +0200

    Added CareCommunication

---
+++ carecommunication/2.1.0/Practitioner/b12c1357-62f1-455e-b07a-3ebf454c50bc/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Practitioner-b12c1357-62f1-455e-b07a-3ebf454c50bc.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Practitioner-b12c1357-62f1-455e-b07a-3ebf454c50bc.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Practitioner-b12c1357-62f1-455e-b07a-3ebf454c50bc.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Practitioner-b12c1357-62f1-455e-b07a-3ebf454c50bc.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Practitioner-b12c1357-62f1-455e-b07a-3ebf454c50bc.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Practitioner-b12c1357-62f1-455e-b07a-3ebf454c50bc.html');
+else 
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Practitioner-b12c1357-62f1-455e-b07a-3ebf454c50bc.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
---
+++ carecommunication/Provenance/4c284936-5454-4116-95fc-3c8eeeed2400/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Provenance-4c284936-5454-4116-95fc-3c8eeeed2400.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Provenance-4c284936-5454-4116-95fc-3c8eeeed2400.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Provenance-4c284936-5454-4116-95fc-3c8eeeed2400.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Provenance-4c284936-5454-4116-95fc-3c8eeeed2400.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Provenance-4c284936-5454-4116-95fc-3c8eeeed2400.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Provenance-4c284936-5454-4116-95fc-3c8eeeed2400.html');
+else 
+  Redirect('http://medcomfhir.dk/ig/carecommunication/2.1.0/Provenance-4c284936-5454-4116-95fc-3c8eeeed2400.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
