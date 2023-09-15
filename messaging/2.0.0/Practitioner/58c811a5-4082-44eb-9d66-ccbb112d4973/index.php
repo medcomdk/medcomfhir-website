10b2501df52a0f1ce31ce6cbeae7c6c8c980e4ba
tmsMedcom <88831880+tmsMedcom@users.noreply.github.com>
2023-09-15 13:12:09 +0200

    Added Messaging

---
+++ messaging/2.0.0/Practitioner/58c811a5-4082-44eb-9d66-ccbb112d4973/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/messaging/2.0.0/Practitioner-58c811a5-4082-44eb-9d66-ccbb112d4973.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://medcomfhir.dk/ig/messaging/2.0.0/Practitioner-58c811a5-4082-44eb-9d66-ccbb112d4973.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://medcomfhir.dk/ig/messaging/2.0.0/Practitioner-58c811a5-4082-44eb-9d66-ccbb112d4973.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/messaging/2.0.0/Practitioner-58c811a5-4082-44eb-9d66-ccbb112d4973.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://medcomfhir.dk/ig/messaging/2.0.0/Practitioner-58c811a5-4082-44eb-9d66-ccbb112d4973.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://medcomfhir.dk/ig/messaging/2.0.0/Practitioner-58c811a5-4082-44eb-9d66-ccbb112d4973.html');
+else 
+  Redirect('http://medcomfhir.dk/ig/messaging/2.0.0/Practitioner-58c811a5-4082-44eb-9d66-ccbb112d4973.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
