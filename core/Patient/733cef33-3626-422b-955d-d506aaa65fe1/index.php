acad5bb6082037acca9606021aa0ae9b8a49c25a
tmsMedcom <88831880+tmsMedcom@users.noreply.github.com>
2023-10-06 14:13:18 +0200

    Revert "updated core"
    This reverts commit 2bbd2470bfe182edb1ca13511f7b64670a181252.

--- core/Patient/733cef33-3626-422b-955d-d506aaa65fe1/index.php
+++ core/Patient/733cef33-3626-422b-955d-d506aaa65fe1/index.php
@@ -7,19 +7,19 @@ function Redirect($url)
 
 $accept = $_SERVER['HTTP_ACCEPT'];
 if (strpos($accept, 'application/json+fhir') !== false)
-  Redirect('http://medcomfhir.dk/ig/carecommunication/3.0.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.json2');
+  Redirect('http://medcomfhir.dk/ig/core/2.1.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.json2');
 elseif (strpos($accept, 'application/fhir+json') !== false)
-  Redirect('http://medcomfhir.dk/ig/carecommunication/3.0.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.json1');
+  Redirect('http://medcomfhir.dk/ig/core/2.1.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.json1');
 elseif (strpos($accept, 'json') !== false)
-  Redirect('http://medcomfhir.dk/ig/carecommunication/3.0.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.json');
+  Redirect('http://medcomfhir.dk/ig/core/2.1.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.json');
 elseif (strpos($accept, 'application/xml+fhir') !== false)
-  Redirect('http://medcomfhir.dk/ig/carecommunication/3.0.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.xml2');
+  Redirect('http://medcomfhir.dk/ig/core/2.1.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.xml2');
 elseif (strpos($accept, 'application/fhir+xml') !== false)
-  Redirect('http://medcomfhir.dk/ig/carecommunication/3.0.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.xml1');
+  Redirect('http://medcomfhir.dk/ig/core/2.1.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.xml1');
 elseif (strpos($accept, 'html') !== false)
-  Redirect('http://medcomfhir.dk/ig/carecommunication/3.0.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.html');
+  Redirect('http://medcomfhir.dk/ig/core/2.1.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.html');
 else 
-  Redirect('http://medcomfhir.dk/ig/carecommunication/3.0.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.xml');
+  Redirect('http://medcomfhir.dk/ig/core/2.1.0/Patient-733cef33-3626-422b-955d-d506aaa65fe1.xml');
 ?>
     
 You should not be seeing this page. If you do, PHP has failed badly.
