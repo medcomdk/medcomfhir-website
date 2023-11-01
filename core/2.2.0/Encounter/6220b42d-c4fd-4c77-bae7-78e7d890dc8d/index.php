e5e01cf4f4e40a6186c21327b089dd0970dbdcce
tmsMedcom <88831880+tmsMedcom@users.noreply.github.com>
2023-11-01 15:50:47 +0100

    Core 2.3.0 og terminology 1.5.0 tilføjet

---
+++ core/2.2.0/Encounter/6220b42d-c4fd-4c77-bae7-78e7d890dc8d/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.2.0/Encounter-6220b42d-c4fd-4c77-bae7-78e7d890dc8d.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.2.0/Encounter-6220b42d-c4fd-4c77-bae7-78e7d890dc8d.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.2.0/Encounter-6220b42d-c4fd-4c77-bae7-78e7d890dc8d.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.2.0/Encounter-6220b42d-c4fd-4c77-bae7-78e7d890dc8d.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.2.0/Encounter-6220b42d-c4fd-4c77-bae7-78e7d890dc8d.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.2.0/Encounter-6220b42d-c4fd-4c77-bae7-78e7d890dc8d.html');
+else 
+  Redirect('http://medcomfhir.dk/ig/core/2.2.0/Encounter-6220b42d-c4fd-4c77-bae7-78e7d890dc8d.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
---
+++ core/2.3.0/Media/cb277ee2-5d96-4762-829d-d36449b2b17f/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.3.0/Media-cb277ee2-5d96-4762-829d-d36449b2b17f.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.3.0/Media-cb277ee2-5d96-4762-829d-d36449b2b17f.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.3.0/Media-cb277ee2-5d96-4762-829d-d36449b2b17f.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.3.0/Media-cb277ee2-5d96-4762-829d-d36449b2b17f.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.3.0/Media-cb277ee2-5d96-4762-829d-d36449b2b17f.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.3.0/Media-cb277ee2-5d96-4762-829d-d36449b2b17f.html');
+else 
+  Redirect('http://medcomfhir.dk/ig/core/2.3.0/Media-cb277ee2-5d96-4762-829d-d36449b2b17f.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
