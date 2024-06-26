b9a6686bbff04ad6502306a498add63a85e62058
tmsMedcom <88831880+tmsMedcom@users.noreply.github.com>
2024-06-26 14:49:48 +0200

    Updated Terminology to 1.7.0

---
+++ ig/terminology/CodeSystem/medcom-messagedefinition-identifer-system/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/terminology/1.7.0/CodeSystem-medcom-messagedefinition-identifer-system.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://medcomfhir.dk/ig/terminology/1.7.0/CodeSystem-medcom-messagedefinition-identifer-system.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://medcomfhir.dk/ig/terminology/1.7.0/CodeSystem-medcom-messagedefinition-identifer-system.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/terminology/1.7.0/CodeSystem-medcom-messagedefinition-identifer-system.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://medcomfhir.dk/ig/terminology/1.7.0/CodeSystem-medcom-messagedefinition-identifer-system.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://medcomfhir.dk/ig/terminology/1.7.0/CodeSystem-medcom-messagedefinition-identifer-system.html');
+else 
+  Redirect('http://medcomfhir.dk/ig/terminology/1.7.0/CodeSystem-medcom-messagedefinition-identifer-system.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
