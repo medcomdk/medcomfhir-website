8f551b13469b7caa28f3c8376999fda206163c91
tmsMedcom <88831880+tmsMedcom@users.noreply.github.com>
2024-06-27 14:26:47 +0200

    Added MedCom v. 2.4.0

---
+++ ig/core/Patient/ded1fc9a-aa93-4162-aaec-7145a8ac951f/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.4.0/Patient-ded1fc9a-aa93-4162-aaec-7145a8ac951f.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.4.0/Patient-ded1fc9a-aa93-4162-aaec-7145a8ac951f.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.4.0/Patient-ded1fc9a-aa93-4162-aaec-7145a8ac951f.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.4.0/Patient-ded1fc9a-aa93-4162-aaec-7145a8ac951f.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.4.0/Patient-ded1fc9a-aa93-4162-aaec-7145a8ac951f.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://medcomfhir.dk/ig/core/2.4.0/Patient-ded1fc9a-aa93-4162-aaec-7145a8ac951f.html');
+else 
+  Redirect('http://medcomfhir.dk/ig/core/2.4.0/Patient-ded1fc9a-aa93-4162-aaec-7145a8ac951f.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
