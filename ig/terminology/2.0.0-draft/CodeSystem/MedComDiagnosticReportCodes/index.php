858048980d37be1d312464e0a1a16ba5eb80a0da
olw-medcom <olw-medcom@users.noreply.github.com>
2025-07-07 12:22:51 +0000

    chore: publish terminology IG 2.0.0-draft

---
+++ ig/terminology/2.0.0-draft/CodeSystem/MedComDiagnosticReportCodes/index.php
@@ -0,0 +1,25 @@
+<?php
+function Redirect($url)
+{
+  header('Location: ' . $url, true, 302);
+  exit();
+}
+
+$accept = $_SERVER['HTTP_ACCEPT'];
+if (strpos($accept, 'application/json+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/terminology/2.0.0-draft/CodeSystem-MedComDiagnosticReportCodes.json2');
+elseif (strpos($accept, 'application/fhir+json') !== false)
+  Redirect('http://medcomfhir.dk/ig/terminology/2.0.0-draft/CodeSystem-MedComDiagnosticReportCodes.json1');
+elseif (strpos($accept, 'json') !== false)
+  Redirect('http://medcomfhir.dk/ig/terminology/2.0.0-draft/CodeSystem-MedComDiagnosticReportCodes.json');
+elseif (strpos($accept, 'application/xml+fhir') !== false)
+  Redirect('http://medcomfhir.dk/ig/terminology/2.0.0-draft/CodeSystem-MedComDiagnosticReportCodes.xml2');
+elseif (strpos($accept, 'application/fhir+xml') !== false)
+  Redirect('http://medcomfhir.dk/ig/terminology/2.0.0-draft/CodeSystem-MedComDiagnosticReportCodes.xml1');
+elseif (strpos($accept, 'html') !== false)
+  Redirect('http://medcomfhir.dk/ig/terminology/2.0.0-draft/CodeSystem-MedComDiagnosticReportCodes.html');
+else 
+  Redirect('http://medcomfhir.dk/ig/terminology/2.0.0-draft/CodeSystem-MedComDiagnosticReportCodes.xml');
+?>
+    
+You should not be seeing this page. If you do, PHP has failed badly.
