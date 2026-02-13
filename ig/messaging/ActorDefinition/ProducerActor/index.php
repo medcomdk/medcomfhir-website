87eee171ee5fb35c31ec67febfbc79c44f6155ad
Oliver Winther (MedCom) <olw@medcom.dk>
2026-02-13 10:39:43 +0100

    Revert "chore: publish messaging IG 4.0.2 (#67)"
    This reverts commit 1624cca2b8de6fe5ac399ee88054479a6cda1df2.

--- ig/messaging/ActorDefinition/ProducerActor/index.php
+++ ig/messaging/ActorDefinition/ProducerActor/index.php
@@ -7,19 +7,19 @@ function Redirect($url)
 
 $accept = $_SERVER['HTTP_ACCEPT'];
 if (strpos($accept, 'application/json+fhir') !== false)
-  Redirect('http://medcomfhir.dk/ig/messaging/4.0.2/ActorDefinition-ProducerActor.json2');
+  Redirect('http://medcomfhir.dk/ig/messaging/4.0.1/ActorDefinition-ProducerActor.json2');
 elseif (strpos($accept, 'application/fhir+json') !== false)
-  Redirect('http://medcomfhir.dk/ig/messaging/4.0.2/ActorDefinition-ProducerActor.json1');
+  Redirect('http://medcomfhir.dk/ig/messaging/4.0.1/ActorDefinition-ProducerActor.json1');
 elseif (strpos($accept, 'json') !== false)
-  Redirect('http://medcomfhir.dk/ig/messaging/4.0.2/ActorDefinition-ProducerActor.json');
+  Redirect('http://medcomfhir.dk/ig/messaging/4.0.1/ActorDefinition-ProducerActor.json');
 elseif (strpos($accept, 'application/xml+fhir') !== false)
-  Redirect('http://medcomfhir.dk/ig/messaging/4.0.2/ActorDefinition-ProducerActor.xml2');
+  Redirect('http://medcomfhir.dk/ig/messaging/4.0.1/ActorDefinition-ProducerActor.xml2');
 elseif (strpos($accept, 'application/fhir+xml') !== false)
-  Redirect('http://medcomfhir.dk/ig/messaging/4.0.2/ActorDefinition-ProducerActor.xml1');
+  Redirect('http://medcomfhir.dk/ig/messaging/4.0.1/ActorDefinition-ProducerActor.xml1');
 elseif (strpos($accept, 'html') !== false)
-  Redirect('http://medcomfhir.dk/ig/messaging/4.0.2/ActorDefinition-ProducerActor.html');
+  Redirect('http://medcomfhir.dk/ig/messaging/4.0.1/ActorDefinition-ProducerActor.html');
 else 
-  Redirect('http://medcomfhir.dk/ig/messaging/4.0.2/ActorDefinition-ProducerActor.xml');
+  Redirect('http://medcomfhir.dk/ig/messaging/4.0.1/ActorDefinition-ProducerActor.xml');
 ?>
     
 You should not be seeing this page. If you do, PHP has failed badly.
